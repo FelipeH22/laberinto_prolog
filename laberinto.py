77b773c78cec1818b6fd61c492e74f7a83323c70
Esteban <juanesteban199911@gmail.com>
2020-07-22 15:19:43 -0500

    Función agrupar filas

--- laberinto.py
+++ laberinto.py
@@ -1,19 +1,20 @@
 from pyswip import Prolog
 def p_fila(lis,lista):
     if lis == []:
-        return lista
-    temp=lis[0]
-    for i in range(0,len(temp)-1):
-        if(temp[i] != "|"):
-            if(temp[i+1]!="|"):
+        return None
+    else:
+        temp=lis[0]
+        for i in range(0,len(temp)-1):
+            if temp[i] != "|" and temp[i+1]!="|":
                 lista += [[temp[i],temp[i+1]]]
-    p_fila(lis[1:],lista)
+        p_fila(lis[1:],lista)
+    return lista
 
 def abre_archivo(archivo_txt):
     return [[elemento for elemento in x if(elemento!='\n' and elemento!=' ')]for x in open(archivo_txt,"r").readlines() ]
     
 lis = abre_archivo("laberinto.txt")
-print(lis)
+#print(lis)
 print(p_fila(lis,[]))
 prolog = Prolog()
 prolog.consult('laberinto.pl')
