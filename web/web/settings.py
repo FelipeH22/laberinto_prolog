15b7a00bc13b223419b78b169a30940d0327905d
Cyara <cmyarap@correo.uditrital.edu.co>
2020-07-28 00:28:29 -0500

    Se ajustan estilos

--- web/web/settings.py
+++ web/web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = _
+SECRET_KEY = 'hz6lo3&13vtppo5v!#bp&3v(%3$7cs44+^xp(#w_adh4^yl_5o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- web/web/vistas.py
+++ web/web/vistas.py
@@ -1,23 +1,33 @@
 from django.http import HttpResponse
 from django.template import Template,Context
+from django.shortcuts import render
 from web import laberinto
 
 def muestra_laberinto(request):
-    archivo = open("C:/Users/jfher/Desktop/2020-1/Modelos 2/laberinto_prolog/web/templates/laberinto.html")
-    plt = Template(archivo.read())
-    archivo.close()
-    ctx=Context()
-    documento=plt.render(ctx)
-    return HttpResponse(documento)
+    laberinto = [
+        [1,2,3,4,5,6],
+        [7,8,9,10,11,12],
+        [13,14,15,16,17,18],
+        [19,20,21,22,23,24],
+        [25,26,27,28,29,30],
+        [31,32,33,34,35,36],
+    ]
+    return render(request, 'laberinto.html', {'laberinto':laberinto})
+
 def resuelve_laberinto(request):
+    laberint = [
+        [1,2,3,4,5,6],
+        [7,8,9,10,11,12],
+        [13,14,15,16,17,18],
+        [19,20,21,22,23,24],
+        [25,26,27,28,29,30],
+        [31,32,33,34,35,36],
+    ]
     cadena = laberinto.lista_def
-    archivo = open("C:/Users/jfher/Desktop/2020-1/Modelos 2/laberinto_prolog/web/templates/solucion.html",'a+')
+    archivo = []
     for linea in cadena:
-        archivo.write(str(linea))
-    archivo.close()
-    archivo = open("C:/Users/jfher/Desktop/2020-1/Modelos 2/laberinto_prolog/web/templates/solucion.html")
-    plt = Template(archivo.read())
-    archivo.close()
-    ctx = Context()
-    documento = plt.render(ctx)
-    return HttpResponse(documento)
+        archivo.append(str(linea))
+    archivo.remove('f')
+    archivo.remove('i')
+    archivo=list(map(int, archivo))
+    return render(request, 'solucion.html', {'archivo':archivo,'laberinto':laberint})
